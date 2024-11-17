a591d1917c7f80829944db7fe2d8e5dffb81dd4f
Ryan Chung <87027981+ryangchung@users.noreply.github.com>
2024-11-17 17:34:16 +0000

    fix: add repository instead of submodule

---
+++ chrome_extension/src/App.tsx
@@ -0,0 +1,25 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h2>Unproductivity Blocker</h2>
+      <div>
+        <p>
+          We are watching your every move. Please don't disappoint us! :)
+        </p>
+      </div>
+      <a href='https://github.com/ryangchung/computer-vision'>
+        Click here to see the repository
+      </a>
+    </>
+  )
+}
+
+export default App

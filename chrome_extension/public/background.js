d24e1e2dcc29a393983d92c6e8be93ffda2ac449
Ryan Chung <87027981+ryangchung@users.noreply.github.com>
2024-11-18 15:16:51 +0000

    feat(ui): completed chrome extension

--- chrome_extension/public/background.js
+++ chrome_extension/public/background.js
@@ -1,13 +1,26 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    const shouldBlock = true;
+const API_URL = "http://127.0.0.1:8000/check-productivity";
+const REDIRECT_URL = chrome.runtime.getURL("blocked.html");
 
-    if (shouldBlock) {
-      return { redirectUrl: chrome.runtime.getURL("blocked.html") };
-    } else {
-      return { cancel: false };
+chrome.webNavigation.onCompleted.addListener(async (details) => {
+  try {
+    const { url } = details;
+
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url: url }),
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+
+      if (result && result.productive === false && result.url) {
+        chrome.tabs.update(details.tabId, { url: result.url });
+      }
     }
-  },
-  { urls: ["<all_urls>"] },
-  ["blocking"]
-);
+  } catch (error) {
+    console.error("Error checking productivity:", error);
+  }
+});
